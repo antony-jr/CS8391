e1c9c21fe8651978fc2f08d29273b5ed7be560b5
antony-jr <antonyjr@protonmail.com>
2019-08-18 17:05:32 +0530

    add Makefile for avl tree and speed up the gen and test combination

--- avl_tree/gen_input.py
+++ avl_tree/gen_input.py
@@ -10,7 +10,7 @@ a = 0
 try:
     a = int(sys.argv[1]);
 except Exception as e:
-    print("3"); # Tell the progrom to exit.
+    print("4"); # Tell the progrom to exit.
     print(e);
     sys.exit(-1)
 
@@ -18,4 +18,5 @@ for i in range(0 , a):
     print("1"); # Tell the program to insert new node
     print(randint(0 , 2**10)); # Generate a random value and insert it
 
-print("3") # quit the program
+print("3") # print the tree
+print("4") # tell the program to exit
--- avl_tree/main.c
+++ avl_tree/main.c
@@ -23,29 +23,30 @@ int main(int ac, char **av){
 	avl_tree_t *tree = NULL;
 	int opt = 0;
 	int value = 0;
-	while(opt != 3){
+	while(opt != 4){
 		printf(":: OPERATIONS ::\n");
 		printf("   1. Insert    \n");
 		printf("   2. Delete    \n");
-		printf("   3. Exit      \n");
+		printf("   3. Display   \n");
+		printf("   4. Exit      \n");
 		printf("Enter your choice: ");
 		scanf("%d" , &opt);
 		if(opt == 1){
-			printf("\n\n");
 			printf("Enter the node value to insert: ");
 			scanf("%d" , &value);
 			tree = avl_tree_insert(tree, value);
-			avl_tree_print(tree, 0);
 			printf("\n\n");
 		}else if(opt == 2){
-			printf("\n\n");
 			printf("Enter the node value to delete: ");
 			scanf("%d" , &value);
 			tree = avl_tree_delete(tree, value);
-			avl_tree_print(tree, 0);
 			printf("\n\n");
+		}else if(opt == 3){
+			printf("\n\n\n");
+			avl_tree_print(tree, 0);
+			printf("\n\n\n");	
 		}else{
-			opt = 3;
+			opt = 4;
 		}
 	}
 	putchar('\n');
